685a36fd11915b31c8754deef359edb2bd43731d
averagefps gamez <mayconner63@gmail.com>
2023-11-29 14:14:32 +0000

    save

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,9 +24,19 @@ var init = function (window) {
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
+            drawCircle();
+            drawCircle();
+            drawCircle();
+            drawCircle();
+            drawCircle();
 
 
         ////////////////////////////////////////////////////////////
